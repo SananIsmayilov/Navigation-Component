f07bde91ad316db077b8abc6d9abaadd57bf6734
Sanan Ismayilov <ismayilovsenan374@gmail.com>
2023-05-09 23:36:27 +0400

    Fragments added

---
+++ app/src/main/java/com/example/navigationproject/SecondFragment.java
@@ -0,0 +1,30 @@
+package com.example.navigationproject;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SecondFragment extends Fragment {
+    Button button2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_second, container, false);
+        button2 = view.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.firstFragment);
+            }
+        });
+        return view;
+    }
+}
